12f01202e93d142f878911c289dd691fd7317c4b
Stanislav <st.kv274@gmail.com>
2023-10-25 02:11:14 +0200

    added bar for external monitor

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,6 +3,15 @@
 killall -q polybar
 
 # Launch bar
+
+hdmi2_status=$(xrandr | grep "HDMI2" | awk '{ print$2 }')
+
 echo "---" | tee -a /tmp/polybar.log
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
+
+if  [[ $hdmi2_status == "connected" ]]; then 
+  polybar bar-external-display 2>&1 | tee -a /tmp/polybar.log & disown
+else 
+  polybar bar 2>&1 | tee -a /tmp/polybar.log & disown
+fi
+    
 echo "Bar launched..."
