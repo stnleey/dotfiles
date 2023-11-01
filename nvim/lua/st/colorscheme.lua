e65e9b8b3144952b7c5006ede035847adebef9c6
Stanislav <st.kv274@gmail.com>
2023-11-01 17:26:42 +0100

    add new way to set colorscheme

--- nvim/lua/st/colorscheme.lua
+++ nvim/lua/st/colorscheme.lua
@@ -1,17 +1,6 @@
--- local colorscheme = "oxocarbon"
--- local colorscheme = "default"
--- local colorscheme = "torte"
--- local colorscheme = "lunar"
--- local colorscheme = "catppuccin-mocha"
-local colorscheme = "aurora"
--- local colorscheme = "sherbet"
--- local colorscheme = "darkplus"
--- local colorscheme = "kyotonight"
--- local colorscheme = "tokyonight-moon"
--- local colorscheme = "gruvbox"
--- local colorscheme = "retrobox"
--- local colorscheme = "lunaperche"
--- local colorscheme = "codemonkey"
+c = require("dynamic")
+
+local colorscheme = Get_colorscheme()
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
